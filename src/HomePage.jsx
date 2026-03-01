344b20848fa2f6b5330540b3be3eb266c026c1a2
linomloka <linoxmloka@gmail.com>
2026-03-01 04:23:03 +0300

    Change HTML into React project.

---
+++ src/HomePage.jsx
@@ -0,0 +1,42 @@
+
+function HomePage() {
+    return (
+        <section className="section">
+            <div className="section_head">
+                <h1>Welcome! <span className="school_head">WILLIALMO SCHOOL</span></h1>
+                <h2>This is where talents and innovations meet conducive environment and advanced technology</h2>
+            </div>
+
+            <div className="min_section">
+                <div className="min_section1">
+                    <div className="min_section1_div1">
+
+                        <h2>Good working environment</h2>
+                        <p>Large classrooms available with enough space to ensure comfort, concentration, and effective learning for all students.</p>
+                        <button className="min_section_buttons">Learn More</button>
+                    </div>
+                    <div className="min_section1_div2">
+                        <h2>Best teachers</h2>
+                        <p>Specialists in transferring knowledge through clear teaching methods and practical understanding.</p>
+                        <button className="min_section_buttons">Learn More</button>
+                    </div>
+                </div>
+
+                <div className="min_section2">
+                    <div className="min_section2_div1">
+                        <h2>Sports</h2>
+                        <p>Discovering talents of our studens in every aspects and develop to enhance good teamwork and good health.</p>
+                        <button className="min_section_buttons">Learn More</button>
+                    </div>
+                    <div className="min_section2_div2">
+                        <h2>Advanced technology</h2>
+                        <p>Inspires studets to change the world by acquiring knowledge and inspire them to think out of the box and be a game changers of nowadays.</p>
+                        <button className="min_section_buttons">Learn More</button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    );
+}
+export default HomePage 
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar(){
+    return(
+         <header>
+        <div className="navbar">
+            <h2 className="school_head">WILLIALMO SCHOOL</h2>
+            <div className="navbar_buttons">
+                <button href="register.html">Register</button>
+                <button href="sign_up.html">Sign Up</button>
+            </div>
+        </div>
+    </header>
+    );
+}
+export default Navbar 
