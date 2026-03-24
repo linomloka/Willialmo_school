dae9d6aae5a1dbbf49f8b886fc9279d8a132338a
linomloka <linoxmloka@gmail.com>
2026-03-24 11:24:44 +0300

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+    return(
+    <footer>
+        <h3>Contact us:</h3>
+        <p>Phone: +255747945986</p>
+        <p>Email: linoxmloka@gmail.com</p>
+        <div class="footer_details">
+            <p>All rights reserved.</p>
+            <p><b>willialmoschool @2026</b></p>
+        </div>
+    </footer>
+    );
+}
+export default Footer
